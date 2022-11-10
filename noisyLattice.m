14727df333d2858a242e2b4073fa74dcc3452e3b
Tomas Ortega <tomas.o.sc@gmail.com>
2022-11-10 14:05:36 -0800

    Update noisyLattice.m

--- noisyLattice.m
+++ noisyLattice.m
@@ -1,42 +1,56 @@
-clear; clc;
-side = 5;
+clear; clc; close all;
+format long;
+
+%% Generate noisy measurements
+side = 10;
 n = side*side; % has to be a perfect square cause I'm lazy
-trueV1 = [0; 1];
-trueV2 = [1; 0];
-noiseSigma = 0.05;
-%% Initialize lattice
-startLattice = zeros(2, n);
+trueV1 = [1; 11];
+trueV2 = [11; -1];
+noiseSigma = 0.3;
+noisyMes = zeros(2, n); % noisy measurements
 z = 1;
 for i = 1:side
     for j = 1:side
-        startLattice(:,z) = [i; j];
+        noisyMes(:,z) = [trueV1, trueV2] * [i; j];
         z = z + 1;
     end
 end
-startLattice = startLattice + randn(2, n) * noiseSigma;
-x = startLattice(1, :);
-y = startLattice(2, :);
-scatter(x, y);
+noisyMes = noisyMes + randn(2, n) * noiseSigma;
+x = noisyMes(1, :);
+y = noisyMes(2, :);
+scatter(x, y,'linewidth', 4,'MarkerEdgeColor', 'g');
 
-%% Now we will do successive optimization
-or = startLattice(:, 2); % I choose it deterministically cause I'm lazy, you will want to do it different
-v1 = [0.01; 0.99];
-v2 = [0.99; -0.01];
-latticeCoords = zeros(2, n);
+%% Create good initial guess
+or = noisyMes(:, 20); % Maybe you will want to do it different
+diffs = noisyMes(:, 2:n) - noisyMes(:, 1:n-1);
+v1 = [median(diffs(1)); median(diffs(2))]; % You want to do something better than this
+v2 = [-v1(2); v1(1)];
 
-nIterations = 20; % I'm lazy and I do not want to make a stopping criteria
+%% Successive optimization
+coords = zeros(2, n);
+nIterations = 3; % I'm lazy and I do not want to make a stopping criteria
 for it = 1:nIterations
+    disp("Iteration number " + it)
     % Start optimizing the coordinates
     for z = 1:n
-        % Again, I'm lazy, this is quadratic and can be done constant
+        distInit = norm(or + coords(1, z)*v1 + coords(2, z)*v2 - noisyMes(:, z));
+        % The true integer coordinates have to be in a +-1 distance from
+        % the floating point ones
+        lambdas = [0; 0];
+        if (norm(noisyMes(:, z) - or) > 1e-3)
+            lambdas = linsolve([v1, v2], noisyMes(:, z) - or);
+        end
         % I impose coordinates must be between -n and n
-        distInit = norm(or + latticeCoords(1, z)*v1 + latticeCoords(2, z)*v2 - startLattice(:, z));
-        for i = -n:n
-            for j = -n:n
-                distAux = norm(or + i*v1 + j*v2 - startLattice(:, z));
+        intLambdas = round(lambdas);
+        intLambdas(intLambdas > n) = n;
+        intLambdas(intLambdas < -n) = -n;
+        for i = -2:2
+            for j = -2:2
+                auxCoords = [i; j] + intLambdas;
+                distAux = norm(or + [v1, v2] * auxCoords - noisyMes(:, z));
                 if distAux < distInit
                     distInit = distAux;
-                    latticeCoords(:, z) = [i; j];
+                    coords(:, z) = auxCoords;
                 end
             end
         end
@@ -44,35 +58,44 @@ for it = 1:nIterations
 
     % Then we optimize vectors and origin
     x0 = [or(1), or(2), v1(1), v1(2), v2(1), v2(2)];
-    fCost = @(x) cost(x, n, latticeCoords, startLattice);
-    x = fminsearch(fCost, x0);
+    fCost = @(x) cost(x, n, coords, noisyMes);
+    [x, totalSqErr] = fminsearch(fCost, x0);
+    disp("Total square error: " + totalSqErr)
     or = [x(1); x(2)];
     v1 = [x(3); x(4)];
     v2 = [x(5); x(6)];
-    plotLattice(startLattice, latticeCoords, or, v1, v2);
+    % if it == nIterations
+    %   plotLattice(startLattice, latticeCoords, or, v1, v2);
+    % end
 end
 
-function res = plotLattice(startLattice, coords, or, v1, v2)
+%% Plot the result
+plotLattice(noisyMes, coords, or, v1, v2);
+
+%% Helper functions
+
+function res = plotLattice(noisyMes, coords, or, v1, v2)
     % coords is a 2 x n array, n >= 2
     % or is the origin of coordinates
     % v1 and v2 are column vectors, the basis of the lattice
-    x = startLattice(1, :);
-    y = startLattice(2, :);
+    x = noisyMes(1, :);
+    y = noisyMes(2, :);
+    scatter(x, y,'linewidth', 4,'MarkerEdgeColor', 'g');
     hold on;
-    scatter(x, y);
     points = or + coords(1, :) .* v1 + coords(2, :) .* v2;
     x = points(1, :);
     y = points(2, :);
-    scatter(x, y, 'x');
+    scatter(x, y, 'x', 'MarkerFaceColor', 'r', 'LineWidth', 2);
     hold off;
+    axis square;
 end
 
-function res = cost(x, n, latticeCoords, startLattice)
+function res = cost(x, n, coords, noisyMes)
     res = 0;
     or = [x(1); x(2)];
     v1 = [x(3); x(4)];
     v2 = [x(5); x(6)];
     for z = 1:n
-        res = res + norm(or + latticeCoords(1, z)*v1 + latticeCoords(2, z)*v2 - startLattice(:, z))^2;
+        res = res + norm(or + coords(1, z)*v1 + coords(2, z)*v2 - noisyMes(:, z))^2;
     end
 end
